7975608cc09c0ed897cba655381e50c4b573c322
imranhei <imranhossainrana.2014@gmail.com>
2026-06-11 16:30:46 +0600

    home page

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="">
-      Decus World is a premium fashion and clothing ecommerce store. Explore our latest collections and enjoy a seamless shopping experience.
-    </div>
-  );
-}
